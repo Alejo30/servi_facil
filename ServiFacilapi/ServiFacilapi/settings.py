906b79d4fa9972ba37a77675db3ba6f2eb44d7f0
lusho31 <47617208+lusho31@users.noreply.github.com>
2020-04-05 22:47:18 -0500

    bd usuarios y personas

--- ServiFacilapi/ServiFacilapi/settings.py
+++ ServiFacilapi/ServiFacilapi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import ServiFacilapi
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django_filters',
-    'serviFacilApp.apps.ServiFacilCongfig',
+    'serviFacilApp.apps.ServifacilappConfig',
 ]
 
 MIDDLEWARE = [
--- ServiFacilapi/serviFacilApp/models.py
+++ ServiFacilapi/serviFacilApp/models.py
@@ -2,4 +2,37 @@ from djongo import models
 from django import forms
 # Create your models here.
 
+class Direccion(models.Model):
+    calle_principal = models.CharField(max_length=120)
+    calle_secundaria = models.CharField(max_length=120)
+    numero_local = models.CharField(max_length=120, default='S/N')
+
+
+class Persona(models.Model):
+    cedula = models.CharField(max_length=20, unique=True)
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    correo = models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    telefono = models.IntegerField()
+    direccion = models.EmbeddedField(
+        model_container=Direccion
+    )
+
+class TipoUser(models.Model):
+    nombre = models.CharField(max_length=10, unique=True)
+    Descripcion = models.TextField()
+
+
 class Usuarios(models.Model):
+    username = models.CharField(max_length=10, unique=True)
+    password = models.CharField(max_length=20)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    persona = models.OneToOneField(Persona, on_delete=models.CASCADE,
+                                   null=False, blank=False)
+    tipo_ususario = models.OneToOneField(TipoUser, on_delete=models.CASCADE,
+                                   null=False, blank=False)
+
+
+
+
